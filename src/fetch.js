0cdc1a3a6cb6d6fef848c137ff76ba6e8adeae2e
Sam Potts <sam@potts.es>
2018-06-15 17:19:08 +1000

    Fetch error handling

--- src/fetch.js
+++ src/fetch.js
@@ -7,21 +7,37 @@ const CustomFetch = (url, options) => {
     const defaults = {
         mode: 'cors',
         redirect: 'follow',
+        headers: {
+            Accept: 'application/json',
+            'X-Requested-With': 'XMLHttpRequest',
+        },
     };
 
     return new Promise((resolve, reject) => {
         fetch(url, Object.assign(defaults, options))
             .then(response => {
-                response
-                    .json()
-                    .then(json => {
-                        if (json.success) {
-                            resolve(json.data);
-                        } else {
-                            reject(json.errors);
-                        }
-                    })
-                    .catch(reject);
+                if (response.status >= 200 && response.status < 300) {
+                    try {
+                        response
+                            .json()
+                            .then(json => {
+                                if (json.success) {
+                                    resolve(json.data);
+                                } else {
+                                    const error = new Error('Request failed');
+                                    error.errors = json.errors;
+                                    throw error;
+                                }
+                            })
+                            .catch(reject);
+                    } catch (error) {
+                        throw error;
+                    }
+                } else {
+                    const error = new Error(response.statusText || response.status);
+                    error.response = response;
+                    throw error;
+                }
             })
             .catch(reject);
     });
