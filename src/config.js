242b622d85ae042be3a4c1c68c3e0d2a577f39a9
Sam Potts <sam@potts.es>
2018-11-08 11:50:07 +1100

    Keep sdk.selz.com endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import is from './utils/is';
 
-const getBase = env => `https://${!is.empty(env) ? `${env}-` : ''}selz.com/sdk/`;
+const getBase = env => `https://${is.empty(env) ? 'sdk.selz.com' : `${env}-selz.com/sdk`}/`;
 
 const config = {
     urls: {
